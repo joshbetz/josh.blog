fd48aab5974fd7ef0acd6a14ad2313939a5becd5
Josh Betz <j@joshbetz.com>
2019-12-07 10:00:49 -0600

    Handle empty search results

--- index.php
+++ index.php
@@ -1,28 +1,41 @@
 <?php get_header(); ?>
 
 <section id="articles">
-	<?php while ( have_posts() ): the_post(); ?>
-	<div>
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			<aside class="sidebar">
-				<div class="sidebar-contents">
-					<?php if ( ! is_page() ):?>
-					<a class="post-date" href="<?php the_permalink(); ?>"><?php the_time( get_option( 'date_format' ) ); ?></a>
-					<div class="categories">
-						<h3>Categories</h3>
-						<?php echo get_the_category_list(); ?>
+	<?php if ( have_posts() ): ?>
+		<?php while ( have_posts() ): the_post(); ?>
+		<div>
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<aside class="sidebar">
+					<div class="sidebar-contents">
+						<?php if ( ! is_page() ):?>
+						<a class="post-date" href="<?php the_permalink(); ?>"><?php the_time( get_option( 'date_format' ) ); ?></a>
+						<div class="categories">
+							<h3>Categories</h3>
+							<?php echo get_the_category_list(); ?>
+						</div>
+						<div class="tags"><?php echo get_the_tag_list( '<h3>Tags</h3><ul><li>', '</li><li>', '</li></ul>' ); ?></div>
+						<?php endif; ?>
 					</div>
-					<div class="tags"><?php echo get_the_tag_list( '<h3>Tags</h3><ul><li>', '</li><li>', '</li></ul>' ); ?></div>
-					<?php endif; ?>
+				</aside>
+				<div class="content">
+					<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+					<div class="post-content"><?php the_content(); ?></div>
 				</div>
-			</aside>
+			<article>
+		</div>
+		<?php endwhile; ?>
+	<?php else: ?>
+		<section id="fourohfour" class="page">
+			<div class="sidebar"></div>
 			<div class="content">
-				<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-				<div class="post-content"><?php the_content(); ?></div>
+				<p>No posts found.</p>
+				<form>
+					<input type=search name=s placeholder=Search...>
+					<input type=submit value=Search>
+				</form>
 			</div>
-		<article>
-	</div>
-	<?php endwhile; ?>
+		</section>
+	<?php endif; ?>
 </section>
 
 <?php get_footer(); ?>
