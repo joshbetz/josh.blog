36d356095ee496292b1b8968526412c5c71e53ea
Josh Betz <j@joshbetz.com>
2019-12-07 09:51:31 -0600

    Use aside for the sidebar

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<?php while ( have_posts() ): the_post(); ?>
 	<div>
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			<div class="sidebar">
+			<aside class="sidebar">
 				<div class="sidebar-contents">
 					<?php if ( ! is_page() ):?>
 					<a class="post-date" href="<?php the_permalink(); ?>"><?php the_time( get_option( 'date_format' ) ); ?></a>
@@ -15,7 +15,7 @@
 					<div class="tags"><?php echo get_the_tag_list( '<h3>Tags</h3><ul><li>', '</li><li>', '</li></ul>' ); ?></div>
 					<?php endif; ?>
 				</div>
-			</div>
+			</aside>
 			<div class="content">
 				<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 				<div class="post-content"><?php the_content(); ?></div>
