c7f4b43f85e596d06ce1c1381ff14c8de14c0270
Josh Betz <j@joshbetz.com>
2020-12-22 12:12:48 -0600

    Add version bump script

---
+++ bin/bump-version.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+BUMP=${1:-patch}
+
+version=$(grep '^Version' style.css | awk '{ print $2 }')
+major=$(echo $version | awk -F. '{ print $1 }')
+minor=$(echo $version | awk -F. '{ print $2 }')
+patch=$(echo $version | awk -F. '{ print $3 }')
+
+case "$BUMP" in
+	'major')
+		major=$(($major + 1))
+		minor='0'
+		patch='0'
+		;;
+
+	'minor')
+		minor=$(($minor + 1))
+		patch='0'
+		;;
+
+	'patch' | *)
+		patch=$(($patch + 1))
+		;;
+esac
+
+newVersion="$major.$minor.$patch"
+
+echo $newVersion
+sed -i '' -e "s/$version/$newVersion/g" functions.php
+sed -i '' -e "s/$version/$newVersion/g" style.css
