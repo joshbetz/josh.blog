3c415617f97ee6c1c3c30f1fb80d831a0a99efe4
Josh Betz <j@joshbetz.com>
2023-01-03 21:34:00 -0600

    Show excerpt in archive if there is no title

--- templates/archive.php
+++ templates/archive.php
@@ -3,5 +3,11 @@
 		<?php echo posted_on_time_string(); ?>
 	</div>
 
-	<?php the_title( '<span class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></span>' ); ?>
+	<?php
+    	if ( strlen( get_the_title() ) > 0 ) {
+			the_title( '<span class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></span>' );
+	    } else {
+    		the_excerpt();
+    	}
+    ?>
 </header>
