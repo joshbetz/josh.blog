1ede30032e36822e09933d1fc27e08294d47a4e0
Josh Betz <j@joshbetz.com>
2024-06-08 10:48:34 -0500

    Improve archive excerpts

--- templates/archive.php
+++ templates/archive.php
@@ -3,11 +3,12 @@
 		<?php echo posted_on_time_string(); ?>
 	</div>
 
-	<?php
-    	if ( strlen( get_the_title() ) > 0 ) {
-			the_title( '<span class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></span>' );
-	    } else {
-    		the_excerpt();
-    	}
-    ?>
+	<?php if ( strlen( get_the_title() ) > 0 ): ?>
+		<?php the_title( '<span class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></span>' ); ?>
+	<?php else: ?>
+		<?php
+			$excerpt = get_the_excerpt();
+			printf( '<span class="excerpt">%s <a href="%s">#</a></span>', $excerpt, esc_url( get_permalink() ) );
+		?>
+	<?php endif; ?>
 </header>
